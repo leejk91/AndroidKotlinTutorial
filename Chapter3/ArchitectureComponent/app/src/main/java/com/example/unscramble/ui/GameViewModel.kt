1d07790ca4161bd287f8ed6456a30ec3c5a7dc20
이정기 <jk@ubcn.co.kr>
2024-10-30 10:51:39 +0900

    Complete

--- Chapter3/ArchitectureComponent/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ Chapter3/ArchitectureComponent/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import com.example.unscramble.data.SCORE_INCREASE
 import kotlinx.coroutines.flow.update
+import com.example.unscramble.data.MAX_NO_OF_WORDS
 
 class GameViewModel : ViewModel() {
     // Game UI state
@@ -70,12 +71,26 @@ class GameViewModel : ViewModel() {
     }
 
     private fun updateGameState(updatedScore:Int){
-        _uiState.update{currentState -> currentState.copy(
-            isGuessedWordWrong = false,
-            currentScrambledWord = pickRandomWordAndShuffle(),
-            score = updatedScore,
-            currentWordCount = currentState.currentWordCount.inc()
-        )}
+        if (usedWords.size == MAX_NO_OF_WORDS){
+            //Last round in the game
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    score = updatedScore,
+                    isGameOver = true
+                )
+            }
+        } else {
+            // Normal round in the game
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    currentScrambledWord = pickRandomWordAndShuffle(),
+                    score = updatedScore,
+                    currentWordCount = currentState.currentWordCount.inc()
+                )
+            }
+        }
     }
 
     fun skipWord() {
